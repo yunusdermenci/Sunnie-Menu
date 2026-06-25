515fcf3aa07055ff2bea1f7ef0ea7b8787766f33
yunusdermenci <emre.dermencioglu@ug.bilkent.edu.tr>
2026-06-25 15:30:42 +0300

    Configure Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // Firebase Console > Project settings > Your apps > Web app config
 // Fill these values after creating the Firebase project.
 window.SUNNIE_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  appId: ""
+  apiKey: "AIzaSyDoYnKAgev8ZJI82z2Sell4_9LgwhTC7CE",
+  authDomain: "sunnie-menu.firebaseapp.com",
+  projectId: "sunnie-menu",
+  appId: "1:1053108120202:web:73d39c39d0ecabdc235b73"
 };
 
 // Create this email/password user in Firebase Authentication.
